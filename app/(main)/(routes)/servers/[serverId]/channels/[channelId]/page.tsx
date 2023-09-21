9aa02296b12c9be1a426faf9504efa5104842adb
ammar <ammar.ali.haidar.1990@gmail.com>
2023-09-21 17:30:52 +0300

    work

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,10 +1,40 @@
+import ChatHeader from '@/components/chat/chat-header'
+import { currentUser } from '@/lib/current-user'
+import { db } from '@/lib/db'
+import { redirectToSignIn } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-type Props = {}
+type Props = {
+    params:{
+        serverId:string,
+        channelId:string
+    }
+}
 
-const page = (props: Props) => {
+const page =async ({params:{channelId,serverId}}: Props) => {
+
+    const profile = await currentUser()
+
+    if(!profile) return redirectToSignIn()
+const channel = await db.channel.findUnique({
+    where:{
+        id:channelId,
+       
+    }
+})
+
+const member = await db.member.findFirst({where:{
+    serverId:serverId,
+    profileId:profile.id
+}})
+
+if(!channel || !member) return redirect('/')
+
   return (
-    <div>channel Id</div>
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+        <ChatHeader name={channel.name} serverId={channel.serverId} type='channel'/>
+    </div>
   )
 }
 
