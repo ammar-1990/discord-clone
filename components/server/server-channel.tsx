aad33a326847d24a8fe4d6d4add4393fe20fc970
ammar <ammar.ali.haidar.1990@gmail.com>
2023-10-01 21:13:34 +0300

    done

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -30,7 +30,7 @@ const params = useParams()
 const Icon = iconMap[channel.type]
 
 const onClick = ()=>{
-    router.push(`/servers/${params.serverId}/channels/${channel.id}`)
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`)
 }
 
 
@@ -44,10 +44,10 @@ const onAction = (e:React.MouseEvent,action:modalType)=>{
   return (
 <button
 onClick={onClick}
-className={cn('group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1',params.channelId === channel.id && 'bg-zinc-700/20 dark:bg-zinc-700')}
+className={cn('group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1',params?.channelId === channel.id && 'bg-zinc-700/20 dark:bg-zinc-700')}
 >
     <Icon className='flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400' />
-    <p className={cn('line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition',params.channelId === channel.id && 'text-primary dark:text-zinc-200 dark:group-hover:text-white')}>{channel.name}</p>
+    <p className={cn('line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition',params?.channelId === channel.id && 'text-primary dark:text-zinc-200 dark:group-hover:text-white')}>{channel.name}</p>
     {channel.name !=='general' && role !=='GUEST' && (
         <div className='ml-auto flex items-center gap-x-2'>
             <ActionTooltip label='Edit'>
